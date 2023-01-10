dda687660629840f74fb64b2d2d7a055fa387fbb
marijanastasijevic <nastasijevicmarija27@gmail.com>
2023-01-10 10:47:20 +0100

    Add 14_SCOPE

---
+++ 14_SCOPE/script.js
@@ -0,0 +1,35 @@
+// let, var i const
+
+let a = 5;
+var b = 6;
+
+a++;
+b++;
+
+console.log(a, b);
+
+a = 5;
+var b = 10;
+
+console.log(a, b);
+
+
+////////////////////
+
+let x = 10;
+
+var y = 20;
+
+if(true) {
+    let x = 15;
+    var y = 25;
+    console.log(x, y)
+    if(true){
+        console.log(x,y); // 15, 25
+        let x = 17;
+        console.log(x, y); // 17, 25
+    }
+    console.log(x,y)// 15,25
+}
+
+console.log(x, y);
