8dc00162b27f8ae055ba957bda259b5fdcb0f839
marijanastasijevic <nastasijevicmarija27@gmail.com>
2023-02-02 16:42:08 +0100

    add addEventListener for formUpdate

--- 30_CHAT_PROJECT/app.js
+++ 30_CHAT_PROJECT/app.js
@@ -3,8 +3,17 @@ import {ChatUI} from './ui.js'
 
 let formaMessage = document.getElementById('formMessage');
 let inputMessage = document.getElementById('message');
+let formaUpdate = document.getElementById('formUpdate');
+let inputUpdate = document.getElementById('update')
 
+//ako postoji u lokalnoj memoriji setuj na to ime umesto postojeceg a ako ne postoji nista onda setuj na anonimus
 let chatroom = new Chatroom("#js", "Jelena");
+
+let user = 'Anonymus';
+
+if(localStorage.username == true){
+    user = localStorage.username
+}
 
 chatroom.getChats(data => {
     console.log(data)
@@ -27,11 +36,24 @@ formaMessage.addEventListener('submit', e => {
 
     chatroom.addChat(poruka)
     .then(() => {
-        formaMessage.reset()
+        //inputMessage.value = ''
+        formaMessage.reset() //prednost ovoga je ako ima vise input polja resetovace sve
     })
     .catch((err) => {
         console.log(err)
     })
+})
+
+formaUpdate.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let updateUser = inputUpdate.value;
+    chatroom.username = updateUser;
+
+    //setuj u lokalnoj memoriji
+    localStorage.setItem('username', updateUser);
+
+    formaUpdate.reset()
 })
 
 // chatroom2.addChat('Hr trening je sutra u 18h')
